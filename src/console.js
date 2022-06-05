b11c88d8f5d88d40edbecc596a2377beed950cea
Ajay-Jaiswal <100516745+Ajay-Jaiswal@users.noreply.github.com>
2022-06-05 12:44:37 +0530

    Delete console.js

--- src/console.js
+++
@@ -1,5 +0,0 @@
-        let findCartDetails= {items: 10}
-        let totalQuantity = 0;
-        for (let i in findCartDetails.items) {
-           console.log(totalQuantity += findCartDetails.items[i].quantity)
-        };
